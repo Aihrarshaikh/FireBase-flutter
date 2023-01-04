ceec9d261bd7f3957a73e754230d4b7d805c7192
aihrar shaikh <aihrarshaikh1208@gmail.com>
2023-01-04 13:03:03 +0530

    fetching data from firestore

--- lib/Screens/basepage.dart
+++ lib/Screens/basepage.dart
@@ -1,3 +1,4 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,6 @@ final user = FirebaseAuth.instance.currentUser!;
 
 class homescreen extends StatefulWidget {
   homescreen({super.key});
-
   @override
   State<homescreen> createState() => _homescreenState();
 }
@@ -17,29 +17,17 @@ class _homescreenState extends State<homescreen> {
     super.initState();
     initialize();
   }
-  String username = "a";
+  var username = "";
+  var emailu = "";
   void initialize() async{
-    DocumentSnapshot snap = await FirebaseFirestore.instance.collection('Users').doc().get();
-    username = snap.id;
+    DocumentSnapshot snap = await FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid).get();
+    setState((){
+      username = snap['name'];
+      emailu = snap['Email'];
+    });
+    print(snap['name']);
+    print(snap['Email']);
   }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //
-  //   myId = snap['name'];
-    // should be myId = snap.get('name');
- //  void initState() {
- //    super.initState();
- //    getdata();
- //  }
- // void getdata() async{
- //   DocumentSnapshot snap = await FirebaseFirestore.instance.collection('Users').doc(await FirebaseAuth.instance.currentUser!.uid).get() as DocumentSnapshot<Object?>;
- //   username = snap as String;
- //    // setState(() {
- //    //   username = value.data('name').toString();
- //    // });
- //  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -53,7 +41,7 @@ class _homescreenState extends State<homescreen> {
                 backgroundImage: NetworkImage(user.photoURL!),
               ),
               Text(username.toString()),
-              Text(user.email!)
+              Text(emailu.toString())
             ]),
       ),
     );
