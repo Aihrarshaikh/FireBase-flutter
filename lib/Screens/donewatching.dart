799c29725380c1090bb434f1ac5894e5ee2beaf7
aihrar shaikh <aihrarshaikh1208@gmail.com>
2023-01-07 21:22:00 +0530

    Added remove from watched list functionality

--- lib/Screens/donewatching.dart
+++ lib/Screens/donewatching.dart
@@ -9,6 +9,19 @@ class donewatching extends StatefulWidget {
 
 class _donewatchingState extends State<donewatching> {
   @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+  var len = 0;
+  void initialize() async {
+    DocumentSnapshot snap = await FirebaseFirestore.instance.collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid).collection('watched').doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    setState(() {
+      len = snap['done watching'].length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,17 +40,29 @@ class _donewatchingState extends State<donewatching> {
                   return SizedBox(
                     height: 800,
                     child: ListView.builder(
-                        itemCount: snapshot.data?.docs.length,
+                        itemCount: len,
                         itemBuilder: (context,index){
                           DocumentSnapshot data = snapshot.data!.docs[0];
-
-                          return Center(child: Column(
-                            children: [
-                              Text(data['done watching'][0]['name']),
-                              Text(data['done watching'][1]['name']),
-                              Text(data['done watching'][2]['name']),
-                            ],
-                          ));
+                          final item = data['done watching'][index]['name'] ;
+                          return Dismissible(
+                            key: Key(item),
+                            onDismissed: (direction) {
+                              setState(() {
+                               FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid).collection("watched").doc(FirebaseAuth.instance.currentUser!.uid).update({
+                                    "done watching" : FieldValue.arrayRemove([{
+                                      "name" : data['done watching'][index]['name']
+                                    }])
+                               });
+                               initialize();
+                              });
+                              // Then show a snackbar.
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(SnackBar(content: Text('$item dismissed')));
+                            },
+                            child: ListTile(
+                              title: Text(item),
+                            ),
+                          );
                         }),
                   );
                 }
